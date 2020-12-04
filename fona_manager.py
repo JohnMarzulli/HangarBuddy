982f79fe180c66703fefa2752295458fe544f3bd
John Marzulli <john.marzulli@hotmail.com>
2020-12-03 20:17:34 -0800

    Using logging module

--- fona_manager.py
+++ fona_manager.py
@@ -307,6 +307,8 @@ class FonaManager(object):
 
 
 if __name__ == '__main__':
+    import logging
+
     import serial
 
     PHONE_NUMBER = "2061234567"
@@ -317,11 +319,11 @@ if __name__ == '__main__':
         SERIAL_CONNECTION = serial.Serial('/dev/ttyUSB0', 9600)
 
     FONA_MANAGER = FonaManager(
-        HangarBuddyLogger(logging.getLogger("heater"),
-        SERIAL_CONNECTION,
-        fona.DEFAULT_POWER_STATUS_PIN,
-        fona.DEFAULT_RING_INDICATOR_PIN,
-        fona.TIMEZONE_OFFSET)
+        logger=HangarBuddyLogger(logging.getLogger("heater")),
+        serial_connection=SERIAL_CONNECTION,
+        power_status_pin=fona.DEFAULT_POWER_STATUS_PIN,
+        ring_indicator_pin=fona.DEFAULT_RING_INDICATOR_PIN,
+        utc_offset=fona.TIMEZONE_OFFSET))
 
     if not FONA_MANAGER.is_power_on():
         print("Power is off..")
