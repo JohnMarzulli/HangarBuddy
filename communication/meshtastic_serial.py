d959dba411de10c68d97c4dde581db787f10a780
John Marzulli <john.marzulli@hotmail.com>
2025-08-18 09:52:30 -0700

    Allow the messaging manager to reconnect itself.

--- communication/meshtastic_serial.py
+++ communication/meshtastic_serial.py
@@ -9,16 +9,79 @@ from pubsub import pub
 class MeshtasticSerial:
     def __init__(self):
         self._message_queue = []
-        self.meshastic_interface: meshtastic.serial_interface.SerialInterface = (
-            self._connect_to_device()
+        self.id: int = 0
+        self.short_name: str = "Unknown"
+        self.long_name: str = "Unknown"
+        self.device_id: str = "Unknown"
+        self.__meshastic_interface__: meshtastic.serial_interface.SerialInterface = (
+            self.__reconnect__()
         )
-        self.meshastic_interface.onReceive = self._on_receive
-        self.short_name = self.meshastic_interface.getShortName()
-        self.long_name = self.meshastic_interface.getLongName()
-        self.device_id = self.meshastic_interface.myInfo.device_id
-        pub.subscribe(self._on_receive, "meshtastic.receive")
 
-    def _connect_to_device(self) -> meshtastic.serial_interface.SerialInterface:
+    def service(self):
+        """
+        Process incoming messages and handle them.
+        """
+        while not self.__is_connected__():
+            print(f"Lost connection to `{self.long_name}`. Reconnecting...")
+
+            try:
+                self.__reconnect__()
+            except Exception:
+                print("Failed to reconnect. Retrying in 5 seconds...")
+                time.sleep(5)
+                continue
+
+    def send(self, recipient, text):
+        if not self.__meshastic_interface__:
+            raise ConnectionError("Not connected to a Meshtastic device.")
+        try:
+            self.__meshastic_interface__.sendText(text, destinationId=recipient)
+        except Exception as e:
+            raise ConnectionError(f"Failed to send message: {e}") from e
+
+    def get_message_queue(self):
+        messages = self._message_queue.copy()
+        self._message_queue.clear()
+        return messages
+
+    def __is_connected__(self) -> bool:
+        """
+        Check if the Meshtastic device is connected.
+        """
+        is_interface_present: bool = self.__meshastic_interface__ is not None
+        is_connected: bool = (
+            is_interface_present and self.__meshastic_interface__.isConnected.is_set()
+        )
+
+        return is_connected
+
+    def __reconnect__(self) -> meshtastic.serial_interface.SerialInterface:
+        """
+        Close the connection to the Meshtastic device.
+        """
+        if (
+            hasattr(self, "meshastic_interface")
+            and self.__meshastic_interface__ is not None
+        ):
+            self.__meshastic_interface__.close()
+
+        self.__meshastic_interface__: meshtastic.serial_interface.SerialInterface = (
+            self.__connect_to_device__()
+        )
+        self.__meshastic_interface__.onReceive = self.__on_receive__
+        self.short_name = str(self.__meshastic_interface__.getShortName())
+        self.long_name = str(self.__meshastic_interface__.getLongName())
+        self.device_id = f"!{hex(self.__meshastic_interface__.myInfo.my_node_num).replace('0x', '')}"
+        self.id = (
+            self.__meshastic_interface__.configId
+            if self.__meshastic_interface__.configId is not None
+            else 0
+        )
+        pub.subscribe(self.__on_receive__, "meshtastic.receive")
+
+        return self.__meshastic_interface__
+
+    def __connect_to_device__(self) -> meshtastic.serial_interface.SerialInterface:
         ports = [port.device for port in serial.tools.list_ports.comports()]
         for port in ports:
             print(f"Trying to connect to Meshtastic device on {port}...")
@@ -30,6 +93,8 @@ class MeshtasticSerial:
                 # Wait for node info to confirm connection
                 time.sleep(2)
                 if potential_meshastic_interface.myInfo:
+                    print(f"Connected to Meshtastic device on {port}.")
+
                     return potential_meshastic_interface
             except Exception:
                 continue
@@ -37,7 +102,7 @@ class MeshtasticSerial:
             "No Meshtastic device found on any serial port, or all the devices is already connected."
         )
 
-    def _on_receive(self, packet, interface):
+    def __on_receive__(self, packet, interface):
         try:
             # Only queue messages intended for this device
             if (
@@ -51,19 +116,6 @@ class MeshtasticSerial:
                 self._message_queue.append(packet)
         except KeyError as e:
             print(f"Error processing packet: {e}")
-
-    def send(self, recipient, text):
-        if not self.meshastic_interface:
-            raise ConnectionError("Not connected to a Meshtastic device.")
-        try:
-            self.meshastic_interface.sendText(text, destinationId=recipient)
-        except Exception as e:
-            raise ConnectionError(f"Failed to send message: {e}") from e
-
-    def get_message_queue(self):
-        messages = self._message_queue.copy()
-        self._message_queue.clear()
-        return messages
 
 
 if __name__ == "__main__":
