c625caab5d592de1dd38b950cc7bc3b19941b82f
John Marzulli <john.marzulli@hotmail.com>
2025-08-27 22:41:00 -0700

    Clean up and annotate return values.

--- devices/sensors/ds18b20_tempature_sensor.py
+++ devices/sensors/ds18b20_tempature_sensor.py
@@ -1,10 +1,12 @@
 """Module to deal with the SunFounder temperature probe."""
 
 import os
-import time
+import pathlib
 
-from devices.interfaces.temperature_sensor import (TemperatureSensor,
-                                                   celcius_to_farenheit)
+from devices.interfaces.temperature_sensor import (
+    TemperatureSensor,
+    celcius_to_farenheit,
+)
 
 # ---------------------------------------------------------------
 # Note:
@@ -23,6 +25,63 @@ from devices.interfaces.temperature_sensor import (TemperatureSensor,
 # https://www.sunfounder.com/learn/Sensor-Kit-v1-0-for-Raspberry-Pi/lesson-17-ds18b20-temperature-sensor-sensor-kit-v1-0-for-pi.html
 
 
+def __read_sensor__(sensor_id: str) -> float | None:
+    """
+    Reads temperature from sensor and prints to stdout
+    id is the id of the sensor.
+
+    >>> read_sensor(None)
+    >>> read_sensor("1")
+    """
+
+    try:
+        text = pathlib.Path(f"/sys/bus/w1/devices/{sensor_id}/w1_slave").read_text()
+        secondline = text.split("\n")[1]
+        temperaturedata = secondline.split(" ")[9]
+        temperature = float(temperaturedata[2:])
+        temperature /= 1000
+        print(f"Sensor: {sensor_id}" + " : %0.3f C" % temperature)
+        print(f"Sensor: {sensor_id}" + " : %0.3f F" % celcius_to_farenheit(temperature))
+
+        return celcius_to_farenheit(temperature)
+    except Exception:
+        return None
+
+
+def __read_sensors__() -> list[float]:
+    """
+    Reads temperature from all sensors found in /sys/bus/w1/devices/
+    starting with "28-...
+
+    >>> read_sensors()
+    Drivers not available.
+    No sensors found! Check connection.
+    []
+    """
+    temperature_probe_values: list[float] = []
+    driver_files: list[str] = []
+
+    try:
+        driver_files = os.listdir("/sys/bus/w1/devices/")
+    except Exception:
+        print("Drivers not available.")
+        return []
+
+    for driver_file in driver_files:
+        if not driver_file.startswith("28-"):
+            pass
+
+        try:
+            probe_value = __read_sensor__(driver_file)
+
+            if probe_value is not None:
+                temperature_probe_values.append(probe_value)
+        except Exception:
+            print("Failed to read sensor")
+
+    return temperature_probe_values
+
+
 class Ds18b20TempatureSensor(TemperatureSensor):
     def __init__(self):
         super().__init__()
@@ -33,7 +92,7 @@ class Ds18b20TempatureSensor(TemperatureSensor):
         if not self.enabled:
             return None
 
-        temperature_values = read_sensors()
+        temperature_values = __read_sensors__()
         if temperature_values is not None and len(temperature_values) > 0:
             self.current_value = int(temperature_values[0])
         else:
@@ -41,79 +100,6 @@ class Ds18b20TempatureSensor(TemperatureSensor):
             self.enabled = False
 
         return self.current_value
-
-
-def read_sensor(sensor_id):
-    """
-    Reads temperature from sensor and prints to stdout
-    id is the id of the sensor.
-
-    >>> read_sensor(None)
-    >>> read_sensor("1")
-    """
-
-    try:
-        tfile = open("/sys/bus/w1/devices/" + sensor_id + "/w1_slave")
-        text = tfile.read()
-        tfile.close()
-        secondline = text.split("\n")[1]
-        temperaturedata = secondline.split(" ")[9]
-        temperature = float(temperaturedata[2:])
-        temperature /= 1000
-        print(f"Sensor: {sensor_id}" + " : %0.3f C" % temperature)
-        print(f"Sensor: {sensor_id}" + " : %0.3f F" % celcius_to_farenheit(temperature))
-
-        return celcius_to_farenheit(temperature)
-    except:
-        return None
-
-
-def read_sensors():
-    """
-    Reads temperature from all sensors found in /sys/bus/w1/devices/
-    starting with "28-...
-
-    >>> read_sensors()
-    Drivers not available.
-    No sensors found! Check connection.
-    []
-    """
-    temperature_probe_values = []
-
-    try:
-        for driver_file in os.listdir("/sys/bus/w1/devices/"):
-            if driver_file.startswith("28-"):
-                try:
-                    probe_value = read_sensor(driver_file)
-
-                    if probe_value is not None:
-                        temperature_probe_values.append(probe_value)
-                except:
-                    print("Failed to read sensor")
-    except:
-        print("Drivers not available.")
-
-    array_length = 0
-    if temperature_probe_values is not None:
-        array_length = len(temperature_probe_values)
-
-    if array_length == 0:
-        print("No sensors found! Check connection.")
-
-    return temperature_probe_values
-
-
-def loop():
-    """read temperature every second for all connected sensors"""
-    while True:
-        read_sensors()
-        time.sleep(1)
-
-
-# Nothing to cleanup
-def destroy():
-    """Tears down the  object."""
-    pass
 
 
 ##############
