0abc72b93832057351dd5bc08592394653722c13
John Marzulli <john.marzulli@hotmail.com>
2025-09-04 00:40:31 -0700

    Get the real network address.

--- lib/local_debug.py
+++ lib/local_debug.py
@@ -25,5 +25,14 @@ def get_ip_address() -> str:
     """
 
     hostname = socket.gethostname()
-
-    return socket.gethostbyname(hostname)
+    ip_address = socket.gethostbyname(hostname)
+    if ip_address.startswith("127."):
+        # Try to get the first non-loopback IPv4 address
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # Doesn't need to be reachable
+            s.connect(("8.8.8.8", 80))
+            ip_address = s.getsockname()[0]
+        finally:
+            s.close()
+    return ip_address
