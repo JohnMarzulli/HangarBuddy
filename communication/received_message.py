6cc9e70d99a33bd9bc91a1d22e6ea7ac02a9f5d6
John Marzulli <john.marzulli@hotmail.com>
2025-09-19 08:39:40 -0700

    Add time of reception to incoming messages

--- communication/received_message.py
+++ communication/received_message.py
@@ -1,5 +1,11 @@
+from datetime import datetime, timezone
+
 class ReceivedMessage:
+    """
+    DTO for a message received by a device.
+    """
     def __init__(self, sender: str, recipient: str, text: str):
         self.sender = sender
         self.recipient = recipient
         self.text = text
+        self.received_at: datetime = datetime.now(timezone.utc)
