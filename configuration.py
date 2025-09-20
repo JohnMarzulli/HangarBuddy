1a30f0f259fd082b0d7f0df62e02501dabee8f66
John Marzulli <john.marzulli@hotmail.com>
2025-09-19 20:20:00 -0700

    Give configuration a few chances to find the configuration file

--- configuration.py
+++ configuration.py
@@ -2,6 +2,8 @@
 
 # encoding: UTF-8
 
+import os.path
+
 from configparser import ConfigParser
 
 import lib.local_debug as local_debug
@@ -17,7 +19,13 @@ def get_config_file_location():
     './HangarBuddy.config'
     """
 
-    return "./HangarBuddy.config"
+    paths = ["./HangarBuddy.config", "../HangarBuddy.config", "../../HangarBuddy.config"]
+
+    for path in paths:
+        if os.path.exists(path):
+            return path
+
+    raise RuntimeError("Unable to find configuration file")
 
 
 class Configuration(object):
@@ -31,42 +39,30 @@ class Configuration(object):
         return self.__config_parser__.get("SETTINGS", "LOGFILE_DIRECTORY")
 
     def __init__(self):
-        print(f"SETTINGS{get_config_file_location()}")
+        file_path = get_config_file_location()
+        print(f"SETTINGS:{file_path}")
 
         self.__config_parser__: ConfigParser = ConfigParser()
-        self.__config_parser__.read(get_config_file_location(), encoding="utf-8")
+        self.__config_parser__.read(file_path, encoding="utf-8")
+
         self.device_type: str = self.__config_parser__.get("SETTINGS", "DEVICE_TYPE")
         self.relay_pin: int = self.__config_parser__.getint("SETTINGS", "HEATER_PIN")
         self.is_mq2_enabled: bool = self.__config_parser__.getboolean("SETTINGS", "MQ2")
-        self.is_temp_probe_enabled: bool = self.__config_parser__.getboolean(
-            "SETTINGS", "TEMP"
-        )
-        self.is_light_sensor_enabled: bool = self.__config_parser__.getboolean(
-            "SETTINGS", "LIGHT_SENSOR"
-        )
+        self.is_temp_probe_enabled: bool = self.__config_parser__.getboolean("SETTINGS", "TEMP")
+        self.is_light_sensor_enabled: bool = self.__config_parser__.getboolean("SETTINGS", "LIGHT_SENSOR")
         self.hangar_dark: int = self.__config_parser__.getint("SETTINGS", "HANGAR_DARK")
         self.hangar_dim: int = self.__config_parser__.getint("SETTINGS", "HANGAR_DIM")
         self.hangar_lit: int = self.__config_parser__.getint("SETTINGS", "HANGAR_LIT")
-        raw_senders_entry: str = self.__config_parser__.get(
-            "SETTINGS", "ALLOWED_SENDERS"
-        )
+        raw_senders_entry: str = self.__config_parser__.get("SETTINGS", "ALLOWED_SENDERS")
         raw_senders_list: list[str] = raw_senders_entry.split(",")
-        self.allowed_senders: list[str] = [
-            sender.strip() for sender in raw_senders_list if sender.strip()
-        ]
-        self.max_minutes_to_run: int = self.__config_parser__.getint(
-            "SETTINGS", "MAX_HEATER_TIME"
-        )
+        self.allowed_senders: list[str] = [sender.strip() for sender in raw_senders_list if sender.strip()]
+        self.max_minutes_to_run: int = self.__config_parser__.getint("SETTINGS", "MAX_HEATER_TIME")
         self.log_filename: str = f"{self.get_log_directory()}hangar_buddy.log"
-        self.max_message_age: int = self.__config_parser__.getint(
-            "SETTINGS", "OLDEST_MESSAGE_TO_PROCESS"
-        )
+        self.max_message_age: int = self.__config_parser__.getint("SETTINGS", "OLDEST_MESSAGE_TO_PROCESS")
         self.utc_offset = self.__config_parser__.getint("SETTINGS", "UTC_OFFSET")
 
         try:
-            self.test_mode: bool = self.__config_parser__.getboolean(
-                "SETTINGS", "TEST_MODE"
-            )
+            self.test_mode: bool = self.__config_parser__.getboolean("SETTINGS", "TEST_MODE")
         except:
             self.test_mode: bool = False
 
