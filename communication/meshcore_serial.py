851a8fa9bd80e55a0e37d26bf6a5c3b7c4aad1c9
John Marzulli <john.marzulli@hotmail.com>
2025-09-19 21:02:29 -0700

    Use correct comparison

--- communication/meshcore_serial.py
+++ communication/meshcore_serial.py
@@ -100,7 +100,7 @@ class MeshcoreSerial(MessagingDevice):
 
             print(f"Trying to match {ascii_safe_name} to CLEANED:{safe_comparison}")
 
-            if name_to_try == ascii_safe_name:
+            if ascii_safe_name == safe_comparison:
                 return contact["adv_name"]
 
         return ""
